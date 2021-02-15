9204dc250dea42db6e28cae835b17e1ec8084678
Barbara Law <barbara.rm.law@gmail.com>
2021-02-15 11:44:25 -0800

    updated 2_15 challenges

--- 2021_02_15.js
+++ 2021_02_15.js
@@ -3,7 +3,15 @@ function sixToast(num) {
   return Math.abs(num-6)
 }
 
-// 7 kyu = Make techno
+// 7 kyu - Make techno
 function perc (mins) {
  return mins < 1 || mins > 100 || typeof mins != 'number' ? 'invalid track time' : [`${Math.round(30*4*mins)} kicks`, `${Math.round(30*8*mins)} hihats`, `${Math.round(30*2*mins)} claps`]
 };
+
+// 8 kyu - Filter out the geese
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter(bird => !geese.includes(bird));
+}; 
