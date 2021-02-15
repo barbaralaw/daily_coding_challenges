1723e55f0ebb6bf0f02edf8ca1bbc2283b1117b4
Barbara Law <barbara.rm.law@gmail.com>
2021-02-15 11:09:05 -0800

    added 2_15 challenge

--- 2021_02_15.js
+++ 2021_02_15.js
@@ -2,3 +2,8 @@
 function sixToast(num) {
   return Math.abs(num-6)
 }
+
+// 7 kyu = Make techno
+function perc (mins) {
+ return mins < 1 || mins > 100 || typeof mins != 'number' ? 'invalid track time' : [`${Math.round(30*4*mins)} kicks`, `${Math.round(30*8*mins)} hihats`, `${Math.round(30*2*mins)} claps`]
+};
