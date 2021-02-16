83d68c57f006891aa28bae612df27025d0884b2b
Barbara Law <barbara.rm.law@gmail.com>
2021-02-15 22:13:16 -0800

    updated 2_15 challenges

--- 2021_02_15.js
+++ 2021_02_15.js
@@ -14,4 +14,51 @@ function gooseFilter (birds) {
 
   // return an array containing all of the strings in the input array except those that match strings in geese
   return birds.filter(bird => !geese.includes(bird));
-}; 
+};
+
+// 8 kyu - Grasshopper - Variable Assignment Debug
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
+// 7 kyu - Flatten and sort an array
+function flattenAndSort(array) {
+  let flatArray = []
+  for (let i=0; i<array.length; i++) {
+    array[i].forEach(elem => flatArray.push(elem))
+  }
+  return flatArray.sort((a,b)=>a-b)
+}
+
+// 7 kyu - Sort arrays - 1
+sortme = function( names ){
+  return names.sort()
+}
+
+// 7 kyu - Leap Years
+sortme = function( names ){
+  return names.sort()
+}
+
+// 7 kyu - JavaScript Array Filter
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(elem => elem%2 === 0)
+}
+
+// 7 kyu - Predict your age!
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+  let ages = [age1, age2, age3, age4, age5, age6, age7, age8]
+  let multAges = ages.map(num => num*num)
+  return Math.floor(Math.sqrt(multAges.reduce((acc,cur)=>acc+cur))/2)
+}
+
+// 7 kyu - Gauß needs help! (Sums of a lot of numbers)
+function f(n){
+  if (typeof n === 'number') {
+    if (n > 0 && n%1 === 0) {
+      return (n*(n+1))/2
+    }
+  }
+  return false
+};
