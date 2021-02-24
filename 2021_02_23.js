6ccdb02a427d745bf4809c56949fd0fed086f217
Barbara Law <barbara.rm.law@gmail.com>
2021-02-23 22:49:47 -0800

    added 02_23 challenges

---
+++ 2021_02_23.js
@@ -0,0 +1,9 @@
+// 8 kyu - Convert to Binary
+function toBinary(n){
+  return Number(n.toString(2));
+}
+
+// 8 kyu - Capitalization and Mutability
+function capitalizeWord(word) {
+  return word.charAt(0).toUpperCase() + word.substring(1,word.length)
+}
