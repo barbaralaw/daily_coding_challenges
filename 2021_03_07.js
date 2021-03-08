b2fd6d384ad77347ccf4ffd080c18460bff06477
Barbara Law <barbara.rm.law@gmail.com>
2021-03-07 22:41:22 -0800

    added 03_07 challenges

--- 2021_03_07.js
+++ 2021_03_07.js
@@ -3,3 +3,8 @@ function unusualFive() {
   let str = 'aaaaa'
   return str.length;
 }
+
+// 8 kyu - Regexp Basics - is it a digit?
+String.prototype.digit = function() {
+  return /^\d$/.test(this)
+};
