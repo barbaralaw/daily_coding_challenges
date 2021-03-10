930eb8a0b065b332d4b902b14370ef5a0fd4e67b
Barbara Law <barbara.rm.law@gmail.com>
2021-03-09 23:55:15 -0800

    added 03_09 challenges

--- 2021_03_09.js
+++ 2021_03_09.js
@@ -27,3 +27,14 @@ function manageMoney(cash,expenses,rate) {
   }
   return `You still have $${cash.toFixed(2)}`
 }
+
+// 8 kyu - A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+function yourFutureCareer() {
+  let career = Math.random()
+  if (career <= 0.32) {
+    return 'FrontEnd Developer'
+  } else if (career <= 0.65) {
+    return 'BackEnd Developer'
+  }
+  return 'Full-Stack Developer'
+}
