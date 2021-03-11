5ea6776859206f035eb3ecf3312c9cb96810c57d
Barbara Law <barbara.rm.law@gmail.com>
2021-03-11 09:26:28 -0800

    added 03_10 challenges

--- 2021_03_10.js
+++ 2021_03_10.js
@@ -28,3 +28,21 @@ function bald(x){
         break
   }
 }
+
+// 7 kyu - Ironman Triathlon
+function iTri(s){
+  let distance = (140.6-s).toFixed(2)
+  if (s === 0) {
+    return 'Starting Line... Good Luck!'
+  } else if (s <= 2.4) {
+    return {'Swim':`${distance} to go!`}
+  } else if (s <= 114.4) {
+    return {'Bike':`${distance} to go!`}
+  } else if (s < 130.6) {
+    return {'Run':`${distance} to go!`}
+  } else if (s < 140.6) {
+    return {'Run':'Nearly there!'}
+  } else {
+    return `You're done! Stop running!`
+  }
+}
