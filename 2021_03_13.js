18332c029510e3475c6d21628ad89492222c025a
Barbara Law <barbara.rm.law@gmail.com>
2021-03-13 22:10:04 -0800

    added 03_13 challenges

---
+++ 2021_03_13.js
@@ -0,0 +1,12 @@
+// 8 kyu - Total amount of points
+function points(games) {
+  let total = 0;
+  for (let i=0; i<games.length; i++) {
+    if (games[i][0] > games[i][2]) {
+      total += 3;
+    } else if (games[i][0] === games[i][2]) {
+      total++;
+    }
+  }
+  return total;
+}
