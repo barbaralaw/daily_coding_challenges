87a45016174fa473ca1f4aa129b9055c969eb2bc
Barbara Law <barbara.rm.law@gmail.com>
2021-03-14 22:36:44 -0700

    updated 03_14 challenges

--- 2021_03_14.js
+++ 2021_03_14.js
@@ -100,3 +100,87 @@ function noBoringZeros(n) {
   }
   return Number(str)
 }
+
+// 8 kyu - Filling an array (part 1)
+const arr = N => {
+  let myArr = []
+  for (let i=0; i<N; i++) {
+    myArr.push(i)
+  }
+  return myArr
+};
+
+// 8 kyu - Sum of differences in array
+function sumOfDifferences(arr) {
+  arr.sort((a,b)=>b-a);
+  let total = 0;
+  for (let i=0; i<arr.length-1; i++) {
+    total+= arr[i]-arr[i+1];
+  }
+  return total;
+}
+
+// 8 kyu - Grasshopper - Messi Goals
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+// 8 kyu - Total pressure calculation
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+  return (((givenMass1/molarMass1)+(givenMass2/molarMass2))*.082*(temp+273.15))/volume
+}
+
+// 8 kyu - Coefficients of the Quadratic Equation
+function quadratic(x1, x2){
+  return [1, -1*(x1+x2), x1*x2]
+}
+
+// 8 kyu - Subtract the Sum
+function SubtractSum(n){
+  return 'apple'
+}
+
+// 8 kyu - altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+String.prototype.toAlternatingCase = function () {
+  let str = '';
+  for (let i=0; i<this.length; i++) {
+    if (this[i] === this[i].toUpperCase()) {
+      str += this[i].toLowerCase()
+    } else {
+      str += this[i].toUpperCase()
+    }
+  }
+  return str
+}
+
+// 8 kyu - Thinkful - Number Drills: Blue and red marbles
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  return (blueStart-bluePulled)/(blueStart+redStart-bluePulled-redPulled)
+}
+
+// 8 kyu - Enumerable Magic #25 - Take the First N Elements
+function take(arr, n) {
+  return arr.slice(0,n)
+}
+
+// 8 kyu - Draw stairs
+function drawStairs(n) {
+  let stairs = '';
+  for (let i=1; i<n; i++) {
+    let j = i-1
+    while (j > 0) {
+      stairs += ' ';
+      j--
+    }
+    stairs += 'I\n'
+  }
+  let x = n-1
+  while (x > 0) {
+    stairs += ' ';
+    x--
+  }
+  stairs += 'I'
+  return stairs;
+}
