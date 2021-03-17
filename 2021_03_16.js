881e1c3182e7c94a3bf087c15af05d2a62c3a68e
Barbara Law <barbara.rm.law@gmail.com>
2021-03-16 22:28:05 -0700

    added 03_16 challenges

--- 2021_03_16.js
+++ 2021_03_16.js
@@ -77,3 +77,21 @@ function findChildren(dancingBrigade){
   }
   return sorted.join('')
 };
+
+// 6 kyu - Is a number prime?
+function isPrime(num) {
+  if (num <= 3) {
+    return num > 1;
+  }
+  if ((num % 2 === 0) || (num % 3 === 0)) {
+    return false;
+  }
+  let count = 5;
+  while (Math.pow(count, 2) <= num) {
+    if (num % count === 0 || num % (count + 2) === 0) {
+      return false;
+    }
+    count += 6;
+  }
+  return true;
+}
