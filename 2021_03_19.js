c366ddcb812b4ceb6ada5b7444960ad545109542
Barbara Law <barbara.rm.law@gmail.com>
2021-03-19 22:28:25 -0700

    added 03_19 challenges

--- 2021_03_19.js
+++ 2021_03_19.js
@@ -3,3 +3,8 @@ function validateUsr(username) {
   res =  /^[a-z0-9_]{4,16}$/.test(username)
   return res
 }
+
+// 8 kyu - validate code with simple regex
+function validateCode (code) {
+  return /^[123]/.test(code)
+}
