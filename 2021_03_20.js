4a2ab6fcefd337cd7afe1934f997c6981d3bddf4
Barbara Law <barbara.rm.law@gmail.com>
2021-03-20 23:39:03 -0700

    added 03_20 challenges

--- 2021_03_20.js
+++ 2021_03_20.js
@@ -4,3 +4,31 @@ function swapValues(arr) {
     arr[0] = arr[1];
     arr[1] = temp;
 }
+
+// 7 kyu - Coding 3min: Bug in Apple
+function sc(apple){
+  for (let i=0; i<apple.length; i++) {
+    for (let j=0; j<apple[i].length; j++) {
+      if (apple[i][j] === 'B') {
+        return [i,j]
+      }
+    }
+  }
+}
+
+// 8 kyu - Coding 3min: Jumping Dutch Act
+function sc(floor){
+  let yell = '';
+  if (floor <= 1) {
+    return yell;
+  } else {
+    for (let i=0; i<floor-1; i++) {
+      yell += 'Aa~ '
+    }
+    yell += 'Pa!'
+  }
+  if (floor <= 6) {
+    yell += ' Aa!'
+  }
+  return yell
+}
