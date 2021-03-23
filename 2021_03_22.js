1ac4b14d4d04f094cac093a2a43d592b242c3bdb
Barbara Law <barbara.rm.law@gmail.com>
2021-03-22 20:55:09 -0700

    added 03_22 challenges

--- 2021_03_22.js
+++ 2021_03_22.js
@@ -2,3 +2,8 @@
 function potatoes(p0, w0, p1) {
   return Math.floor(w0 * (100-p0)/(100-p1))
 }
+
+// 7 kyu - Vowel Count
+function getCount(str) {
+  return str.split('').filter(x => /^[aeiuo]$/.test(x)).length
+}
