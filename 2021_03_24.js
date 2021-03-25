a951d8770e163d6c1a9f74d422947844ab78c842
Barbara Law <barbara.rm.law@gmail.com>
2021-03-24 23:19:00 -0700

    added 03_24 challenges

--- 2021_03_24.js
+++ 2021_03_24.js
@@ -1,2 +1,24 @@
 // 8 kyu - !a == a?!
 const a = '0'
+
+// 8 kyu - Barking mad
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function() {
+  return "Woof";
+}
+
+// 8 kyu - Be Concise I - The Ternary Operator
+function describeAge(age) {
+  return `You're a(n) `+(age<=12?`kid`:age<=17?`teenager`:age<=64?`adult`:`elderly`)
+}
