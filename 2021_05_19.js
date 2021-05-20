9cca3680c7d5ecb47fe1c441d430b162206faf04
Barbara Law <barbara.rm.law@gmail.com>
2021-05-19 22:05:29 -0700

    added 05_19 challenges

--- 2021_05_19.js
+++ 2021_05_19.js
@@ -4,3 +4,14 @@ function calculate(str) {
   str = str.split('minus').join('+-')
   return str.split('+').reduce((a,c)=>a + Number(c), 0).toString()
 }
+
+// 8 kyu - Shifty Closures
+var name;
+var greet_abe = function() {
+  name = 'Abe'
+  return "Hello, " + name + '!';
+};
+var greet_ben = function() {
+  name = 'Ben'
+  return "Hello, " + name + '!';
+};
