4a73624baec61456a7eb52a32d44d106ae2e16a6
Barbara Law <barbara.rm.law@gmail.com>
2021-05-21 22:40:31 -0700

    added 05_21 challenges

--- 2021_05_21.js
+++ 2021_05_21.js
@@ -34,3 +34,18 @@ function amIWilson(p) {
   }
   return ( (fact + 1) / (p ** 2) ) % 1 ? false : true
 }
+
+// 8 kyu - For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+var quote = function(fighter) {
+  return fighter.toLowerCase() === 'conor mcgregor' ?
+    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    : "I am not impressed by your performance."
+};
+
+// 8 kyu - Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+function remove(s,n){
+  for (let i=0; i<n; i++) {
+    s = s.replace('!','')
+  }
+  return s
+}
