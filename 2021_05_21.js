912c34127903bb3f86498167d7798c7f6fecd0e3
Barbara Law <barbara.rm.law@gmail.com>
2021-05-21 22:01:56 -0700

    added 05_21 challenges

--- 2021_05_21.js
+++ 2021_05_21.js
@@ -2,3 +2,13 @@
 function annulusArea(a) {
   return Number((Math.PI*(Math.pow(a/2,2))).toFixed(2))
 }
+
+// 8 kyu - Pole Vault Starting Marks
+function startingMark(bodyHeight){
+  return Number((3.47 + bodyHeight*3.93442).toFixed(2))
+}
+
+// 8 kyu - Localize The Barycenter of a Triangle
+function barTriang(p1, p2, p3){
+  return [Number(((p1[0]+p2[0]+p3[0])/3).toFixed(4)), Number(((p1[1]+p2[1]+p3[1])/3).toFixed(4))]
+}
