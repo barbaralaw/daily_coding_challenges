fbb98e4b085c59f82e815bf1a2cd8838dff08d6a
Barbara Law <barbara.rm.law@gmail.com>
2021-05-27 23:32:44 -0700

    added 05_27 challenges

---
+++ 2021_05_27.js
@@ -0,0 +1,10 @@
+// 7 kyu - Debug the functions EASY
+function multi(arr) {
+  return arr.reduce((a,c)=>a*c);
+}
+function add(arr) {
+  return arr.reduce((a,c)=>a+c);
+}
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
