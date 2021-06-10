a59a58c8ce5b58efb5d73d175b8df7b6418c7803
Barbara Law <barbara.rm.law@gmail.com>
2021-06-10 09:49:43 -0700

    added 06_10 challenges

--- 2021_06_10.js
+++ 2021_06_10.js
@@ -3,3 +3,8 @@ function solution(value){
   let str = '00000' + String(value)
   return 'Value is ' + str.slice(-5)
 }
+
+// 8 kyu - Hex to Decimal
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
