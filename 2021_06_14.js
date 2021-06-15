a7e3d53eedf934ce624abb9ddf2318436893d260
Barbara Law <barbara.rm.law@gmail.com>
2021-06-14 19:43:35 -0700

    added 06_14 challenges

--- 2021_06_14.js
+++ 2021_06_14.js
@@ -44,3 +44,14 @@ function greetDevelopers(list) {
   }
   return list
 }
+
+// 7 kyu - Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
+function countDevelopers(list) {
+  let count = 0
+  for (let person of list) {
+    if (person.continent == 'Europe' && person.language == 'JavaScript') {
+      count++
+    }
+  }
+  return count
+}
